6af40911db1f56bc6839973e2ebae60688d4c585
epshone <epshone@gmail.com>
2018-01-22 00:38:29 -0500

    initial commit

--- models/test.py
+++ models/test.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, r'/Users/EvanShone/git/cryptkeeper')
 import triggers.test as test
 import logging
 from datetime import timedelta, datetime
@@ -10,9 +12,9 @@ class order_tester(object):
         self.get_order_params_fn = order_class._get_order_params
         self.check_place_order_fn = order_class._check_place_order
         self.evaluate_fn = order_class._evaluate
-        self.monitor_class = order_class.monitor_class
+        self.monitor_class = monitor_class
 
-    def test(self, start_datetime, end_datetime, refresh_rate):
+    def test(self, coin, start_datetime, end_datetime, refresh_rate):
         # TODO get info from API
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
@@ -22,23 +24,28 @@ class order_tester(object):
 
         self.balance = 1000
 
+        self.testing_order_obj = self.order_class(refresh_seconds=refresh_rate,
+                                                  coin=coin,
+                                                  threshold=4)
         self.new_watch()
 
     def get_info_at_datetime(self):
         # get the coin info at self.curr_datetime
-        return 5
+        return 13
 
     def new_evaluate(self):
-        if(self.check_place_order_fn(self.get_info_at_datetime())):
+        if(self.check_place_order_fn(self.testing_order_obj,
+                                     self.get_info_at_datetime())):
             self.new_action()
 
     def new_action(self):
-        logging.debug("Order " + str(8)
-                      + " placed: " + self.get_order_params_fn())
+        logging.debug("Order " + str(8) + " placed: "
+                      + self.get_order_params_fn(self.testing_order_obj))
 
     def new_watch(self):
         while(self.watching):
-            delta = timedelta(seconds=self.seconds)
+            logging.debug("in")
+            delta = timedelta(seconds=self.refresh_rate)
             self.curr_datetime = self.curr_datetime + delta
             if (self.curr_datetime > self.end_datetime):
                 return
@@ -49,9 +56,13 @@ class order_tester(object):
 
 def main():
     tester = order_tester(test.example_order)
-    tester.test(datetime.now() - timedelta(seconds=10), datetime.now(), 1)
+    tester.test(coin="AION/ETH",
+                start_datetime=datetime.now() - timedelta(seconds=10),
+                end_datetime=datetime.now(),
+                refresh_rate=1)
     logging.debug("done")
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='output.log', level=logging.DEBUG)
     main()
