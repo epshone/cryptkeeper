67fdd81eed8e951c07acc0c43c550d00076ff280
makesitlookeasy <kenmrosenberg@comcast.net>
2018-01-22 20:43:22 -0500

    user interface

--- triggers/input.py
+++ triggers/input.py
@@ -1,21 +1,16 @@
 # Command Line Input
 
-import cmd, sys, argparse, json
+import cmd, json
 
 class CommandLineInterface(cmd.Cmd):
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("credentials",type=str, help="Path to api credentials")
-    args = parser.parse_args()
-    credentials = json.load(open(args.credentials))
 
     intro = "\n Welcome to CryptKeeper. Type 'help' to list commands. \n"
     prompt = "(CryptKeeper) "
 
     def do_checkCredentials(self, arg):
         "\nType checkCredentials to display API credentials\n"
-        print "\nAPI Key: " + self.credentials["apiKey"] + "\n"
-        print "Secret Key: " + self.credentials["secret"] + "\n"
+        print "\nAPI Key: " + self.api_key + "\n"
+        print "Secret Key: " + self.api_secret + "\n"
 
     def do_quit(self, arg):
         "\n Type quit to exit CryptKeeper \n"
