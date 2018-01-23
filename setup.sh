dff0ecb98a50a6ebe9f3ca33e15226c4ded3366a
epshone <epshone@gmail.com>
2018-01-22 20:21:42 -0500

    updated setup file

--- setup.sh
+++ setup.sh
@@ -10,7 +10,20 @@ SITEDIR=$(python -m site --user-site)
 # create if it doesn't exist
 mkdir -p "$SITEDIR"
 
+# get the directory
 CURRENTPATH=$(pwd)
 
 # create new .pth file with our path
 echo "$CURRENTPATH" > "$SITEDIR/cryptkeeper.pth"
+
+# create credentials file if it doesn't exist
+if [ -f credentials.json ]; then
+   echo "credentials.json already exists"
+else
+   echo "creating file credentials.json"
+   touch credentials.json
+   echo "{" >> credentials.json
+   echo "    \"API_KEY\": \"\"," >> credentials.json
+   echo "    \"API_SECRET\": \"\"" >> credentials.json
+   echo "}" >> credentials.json
+fi
